159cf83f4888a42b16dd49df1d037115a91bcc19
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2021-05-27 22:14:28 -0400

    Add files via upload

---
+++ template/fullscreen.sh
@@ -0,0 +1,7 @@
+ip=192.168.1.113
+user=me
+pass=u
+xfreerdp /v:$ip /u:$user /p:$pass /cert:ignore /smart-sizing:2560x1440
+#&
+
+# https://github.com/FreeRDP/FreeRDP/issues/5114
---
+++ template/workarea.sh
@@ -0,0 +1,7 @@
+ip=192.168.1.113
+user=me
+pass=u
+xfreerdp /v:$ip /u:$user /p:$pass /cert:ignore /workarea
+#&
+
+# https://github.com/FreeRDP/FreeRDP/issues/5114
