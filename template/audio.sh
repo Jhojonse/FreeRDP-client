bd0602045a28735e5ef43564a65b3a4a513caf8e
Twin-Flame <61005029+Twin-Flame@users.noreply.github.com>
2021-05-28 01:43:13 -0400

    Add files via upload

---
+++ template/audio.sh
@@ -0,0 +1,7 @@
+ip=192.168.1.113
+user=me
+pass=u
+xfreerdp /v:$ip /u:$user /p:$pass /cert:ignore /gfx /workarea /scale:140 /microphone:sys:alsa /sound:sys:alsa /clipboard
+#&
+
+# https://github.com/FreeRDP/FreeRDP/issues/5114
